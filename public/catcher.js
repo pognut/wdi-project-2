895d50c54c16019923d2362f9cf020a781a12d2d
Nicholas Michel <nickmichel100@gmail.com>
2016-08-15 11:54:11 -0400

    functioning question and answer loaders

--- public/catcher.js
+++ public/catcher.js
@@ -1,8 +1,8 @@
 //for catching game
-// ["ques" + api var + "tion"]
-var easyQuestions = ["What is this Pokemon's Pokedex number?"]
+// [{"ques" + api var + "tion": answer path in object}]
+var easyQuestions = [{"What is this Pokemon's Pokedex number?":"id"}, {"What is this Pokemon's weight?":"weight"}]
 
-var easyMons = ["Bulbasaur"]
+var easyMons = ["Bulbasaur", "Charmander"]
 
 // var mediumMons
 
@@ -10,16 +10,43 @@ var easyMons = ["Bulbasaur"]
 
 var questionSelector = function(diff){
   if(diff ===1){
-    var index = Math.floor(Math.random() * (easyQuestions.length + 1));
+    var index = Math.floor(Math.random() * (easyQuestions.length));
     return easyQuestions[index]
   }
 }
 
 var pokemonSelector = function(diff){
   if(diff===1){
-    var index = Math.floor(Math.random() * (easyMons.length + 1));
+    var index = Math.floor(Math.random() * (easyMons.length));
     return easyMons[index]
   }
+}
+
+
+
+var quizMaster = function(data){
+  $('body').append('<div id = quizWindow>');
+  $('#quizWindow').append('<h3 id = questionWindow>');
+  $('#quizWindow').append('<form id = answerWindow>');
+  $('#answerWindow').append('<input id = answerInput>')
+  $('#answerWindow').append('<button id="answerSubmit" type="submit">Submit</button>')
+  var questObj = questionSelector(1);
+  var questStr = Object.keys(questObj);
+  var questAns = questObj[questStr];
+  $('#questionWindow').text(questStr);
+  $('#answerSubmit').on('click', function(e){
+    e.preventDefault();
+    if($('#answerInput').val()==data[questAns]){
+      console.log('right')
+    }
+    else{
+      console.log('wrong')
+    }
+  })
+  //bring up quiz window
+  //fill in questions
+  //record answers
+  //display question difficulty, questions to go, number of wrongs etc.
 }
 
 var pokeInfo = function(diff){
@@ -31,14 +58,12 @@ var pokeInfo = function(diff){
     'success': function(data){
       console.log('get success')
       console.log(data)
+      quizMaster(data);
     }
   })
 }
 
-// var quizMaster
-
-
-
 $('#easy').on('click', function(){
   pokeInfo(1);
+  // quizMaster();
 })
